d453ab12be40e056cbd653952bd8564ee9dcf1be
Nathalie-Sansone <79091574+Nathalie-Sansone@users.noreply.github.com>
2021-04-26 12:11:29 +0200

    Update Speedometer.php

--- Speedometer.php
+++ Speedometer.php
@@ -5,7 +5,7 @@ class Speedometer
 {
     public const ONE_MILE = 0.621;
 
-    public static function convertToMiles(int $km): float
+    public static function convertKmToMiles(int $km): float
     {
         return $km * self::ONE_MILE;
     }
